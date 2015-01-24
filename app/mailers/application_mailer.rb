2bc6f3559d7d98c082f1bea447a20d55c45bd189
Jeff Conway <jconway@arcwebtech.com>
2015-01-23 20:52:50 -0500

    New image; new mailing addresses

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,16 +1,17 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "aoss.no.reply@gmail.com"
-  TO_ADDRESS = "conwayje@gmail.com"
+  quotes_and_service_address = "aossservice@aoss.us"
+  careers_address = "director@aoss.us"
 
   def new_contact_request(contact_request_id)
     @cr = ContactRequest.find(contact_request_id)
-    mail(to: TO_ADDRESS, subject: "[aoss.us] New Contact Request")
+    mail(to: quotes_and_service_address, subject: "[aoss.us] New Contact Request")
   end
 
   def new_employment_application(application_id)
     @app = EmploymentApplication.find(application_id)
     attachments[@app.resume.instance.resume_file_name] = File.read(@app.resume.path)
     
-    mail(to: TO_ADDRESS, subject: "[aoss.us] New Employment Application")
+    mail(to: careers_address, subject: "[aoss.us] New Employment Application")
   end
 end
