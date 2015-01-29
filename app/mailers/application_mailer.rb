f9852e86d1983ad4b7b2d5b4959d98e8832951e0
Jeff Conway <jconway@arcwebtech.com>
2015-01-29 13:26:22 -0500

    A number of small revisions

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,17 +1,20 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "aoss.no.reply@gmail.com"
-  quotes_and_service_address = "aossservice@aoss.us"
-  careers_address = "director@aoss.us"
+
+  @@quotes_and_service_address = "aossservice@aoss.us"
+  @@careers_address = "director@aoss.us"
 
   def new_contact_request(contact_request_id)
     @cr = ContactRequest.find(contact_request_id)
-    mail(to: quotes_and_service_address, subject: "[aoss.us] New Contact Request")
+    mail(to: @@quotes_and_service_address, subject: "[aoss.us] New Contact Request")
   end
 
   def new_employment_application(application_id)
     @app = EmploymentApplication.find(application_id)
-    attachments[@app.resume.instance.resume_file_name] = File.read(@app.resume.path)
-    
-    mail(to: careers_address, subject: "[aoss.us] New Employment Application")
+    if @app.resume.exists?
+      attachments[@app.resume.instance.resume_file_name] = File.read(@app.resume.path)
+    end
+
+    mail(to: @@careers_address, subject: "[aoss.us] New Employment Application")
   end
 end
